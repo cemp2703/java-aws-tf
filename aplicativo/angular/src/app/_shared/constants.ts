ce85b9c5573af53e503fcb5635d255d2ee9052cb
Cesar Palma <cemp2703@gmail.com>
2020-04-24 01:29:09 -0500

    correcciones

--- aplicativo/angular/src/app/_shared/constants.ts
+++ aplicativo/angular/src/app/_shared/constants.ts
@@ -1,10 +1,8 @@
-export const HOST_BACKEND = `http://localhost:8081`
+//export const HOST_BACKEND = `http://localhost:8081`
 //export const HOST_BACKEND = `http://192.168.99.100`
 //export const HOST_BACKEND = `https://a5q8gn7rkb.execute-api.us-east-1.amazonaws.com/prod`
+export const HOST_BACKEND =`http://mybalancercf-1830245007.us-east-1.elb.amazonaws.com`
 
-export const TIME_UPDATE_GEOLOCALIZATION = 60000;
-export const RADIO = 0.029;
-export const ZOOM = 16;
 export const TOKEN_NAME = "idToken";
 export const REFRESH_TOKEN_NAME = "refreshToken";
 export const ACCESS_TOKEN_NAME = "accessToken";
