f0796e2c982c29dcf000fd7667b81b1695584564
Cesar Palma <cemp2703@gmail.com>
2020-04-27 09:45:18 -0500

    Agregando API Gateway, S3 y Cloudfront (subiendo frontend)

--- aplicativo/angular/src/app/_shared/constants.ts
+++ aplicativo/angular/src/app/_shared/constants.ts
@@ -1,7 +1,6 @@
 //export const HOST_BACKEND = `http://localhost:8081`
-//export const HOST_BACKEND = `http://192.168.99.100`
-//export const HOST_BACKEND = `https://a5q8gn7rkb.execute-api.us-east-1.amazonaws.com/prod`
-export const HOST_BACKEND =`http://mybalancercf-1830245007.us-east-1.elb.amazonaws.com`
+//export const HOST_BACKEND =`http://mybalancercf-1830245007.us-east-1.elb.amazonaws.com`
+export const HOST_BACKEND =`https://rk983uneh1.execute-api.us-east-1.amazonaws.com/prod`
 
 export const TOKEN_NAME = "idToken";
 export const REFRESH_TOKEN_NAME = "refreshToken";
